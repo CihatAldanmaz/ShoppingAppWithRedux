4908c6e49db487f23a4128a4e03d92bd2f5ee098
CihatAld <cihataldanmaz@gmail.com>
2020-01-15 05:43:31 -0500

    Active function added

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -16,7 +16,7 @@ class CategoryList extends Component {
     <h3>Categories</h3>
     <ListGroup>
         {this.props.categories.map(category => (
-            <ListGroupItem onClick = {() => this.props.actions.changeCategory(category)} key={category.id} key={category.id}>
+            <ListGroupItem active = {category.id === this.props.currentCategory.id} onClick = {() => this.props.actions.changeCategory(category)} key={category.id} key={category.id}>
                 {category.categoryName}
 </ListGroupItem>
         ))}
