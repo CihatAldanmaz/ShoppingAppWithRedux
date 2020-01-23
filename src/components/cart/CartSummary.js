2152a645cf048d22ef7c55f0af04dd1ac51632b4
CihatAld <cihataldanmaz@gmail.com>
2020-01-23 00:58:45 -0500

    Cart Summary Completed

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -3,25 +3,58 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  NavLink,
+  Badge
 } from "reactstrap";
+import { connect } from "react-redux";
 
-export default class CartSummary extends Component {
+class CartSummary extends Component {
+
+    renderEmptyCart(){
+        return (
+            <NavLink>Your Cart is Empty</NavLink>
+        )
+    }
+
+    renderSummary(){
+        return (
+            <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            Your Cart
+          </DropdownToggle>
+          <DropdownMenu right>
+              {
+                  this.props.cart.map(item =>(
+                  <DropdownItem key={item.product.id}>{item.product.productName}
+                  <Badge color="success">{item.quantity}</Badge>
+                  </DropdownItem>
+                  
+                  ))
+              }
+           
+            <DropdownItem divider />
+            <DropdownItem>Check Out</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        )
+    }
+
   render() {
     return (
       <div>
-        <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret>
-            Options
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>Option 1</DropdownItem>
-            <DropdownItem>Option 2</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Reset</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
+          {
+              this.props.cart.length < 1 ? this.renderEmptyCart() : this.renderSummary()
+          }
+        
       </div>
     );
   }
 }
+
+function mapStateToProps(state){
+    return {
+        cart:state.cartReducer
+    }
+}
+export default connect(mapStateToProps)(CartSummary);
